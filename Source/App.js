992a0d2baf2cecca45241627a772d1b59f4d310a
MarcoB <34425929+Ormodus@users.noreply.github.com>
2023-08-28 15:18:58 +0200

    Update App.js

--- Source/App.js
+++ Source/App.js
@@ -1,197 +1,321 @@
+<!DOCTYPE html>
+<html lang="en">
 
-import { create, byId } from 'Document'
-// import { driver, session } from 'Neo4j'
-
-
-let recognition, currentTopic;
-
-const italianKeywords = [
-    ['intro'],
-    ['matematica', 'scienza', 'fibonacci'],
-    ['università', 'napoli', 'studio', 'lezione'],
-    ['corte', 'possedimenti', 'personaggi'],
-    ['lingue', 'culture', 'testi', 'traduzioni', 'arabo'],
-    ['medico', 'medicina', 'scuola medica salernitana', 'dottori', 'chirurgia'],
-    ['nascita', 'nato', 'stupor Mundi', 'missione'],
-    ['arte', 'poesia', 'poeti', 'scuola', 'siciliana', 'letterati', 'volgare'],
-]
-
-const italianVideos = [
-    "Videos/Intro.mp4",
-    "Videos/Matematica.mp4",
-    "Videos/Universita.mp4",
-    "Videos/Corte.mp4",
-    "Videos/Traduzioni.mp4",
-    "Videos/Medicina.mp4",
-    "Videos/Nascita.mp4",
-    "Videos/Poesia.mp4",
-]
-
-const italianContents = [
-    "Contents/Intro.txt",
-    "Contents/Matematica.txt",
-    "Contents/Universita.txt",
-    "Contents/Corte.txt",
-    "Contents/Traduzioni.txt",
-    "Contents/Medicina.txt",
-    "Contents/Nascita.txt",
-    "Contents/Poesia.txt",
-]
-
-const repeatKeywords = [
-    "ripeti", "ancora", "ricomincia", "nuovo", "repeat"
-]
-
-const socket = byId("socket")
-const inputField = byId("text-input");
-const mic = byId("mic");
-const micWrapper = byId("mic-wrapper");
-const videoPlayer = byId("video-player");
-const clearButton = byId("clear");
-const chatContainer = byId("chat");
-const sendButton = byId("send-button");
-const noAnswer = "Non posso fornire informazioni in merito ;')";
-const greeting = "Ciao, sono Federico II e ti aiuterò in questo percorso"
-const lettersRegex = /[a-zA-Z]+/g;
-
-if ("SpeechRecognition" in window || "webkitSpeechRecognition" in window) {
-    recognition = new (window.SpeechRecognition || window.webkitSpeechRecognition)();
-    recognition.lang = "it-IT";
-    recognition.continuous = true;
-}
-else {
-    recognition = none;
-}
-
-let isMouseDown = false;
-
-
-// Mic button listener
-mic.addEventListener("mousedown", () => {
-    if (recognition) {
-        recognition.start();
-    }
-    isMouseDown = true;
-    micWrapper.style.backgroundColor = "#e9483f";
-});
-
-document.addEventListener("mouseup", () => {
-    if (isMouseDown) {
-        if (recognition) {
-            recognition.stop();
-        }
-        isMouseDown = false;
-        micWrapper.style.backgroundColor = "";
-        recognition.onresult = (event) => {
-            const transcript = event.results[event.results.length - 1][0].transcript;
-            if (/\s/g.test(inputField.value[-1])) {
-                inputField.value += " ";
+<head>
+	<script type="importmap">
+        {
+            "imports": {
+                "Document": "./Misc/Document.js",
+                "Neo4j": "./Misc/Neo4j.js"
             }
-            inputField.value += transcript + " ";
-
-        };
-    }
-});
-
-
-// Typing animation
-function typingMessage(container, message, timeout) {
-    const newMessageDiv = create("div");
-    newMessageDiv.classList.add("out");
-    container.appendChild(newMessageDiv);
-
-    let currentIndex = 0;
-    const typingInterval = setInterval(() => {
-        newMessageDiv.textContent = message.slice(0, currentIndex++);
-        if (newMessageDiv.scrollHeight > newMessageDiv.clientHeight) {
-            newMessageDiv.style.height = newMessageDiv.scrollHeight + "px";
         }
+    </script>
+	<title>Hologram</title>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet"
+		integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+	<link rel='stylesheet' href='//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css'>
+	<link rel='stylesheet' href='//cdnjs.cloudflare.com/ajax/libs/animate.css/3.2.3/animate.min.css'>
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css">
+	<link rel="stylesheet" href="Visual/Icons.css">
+	<link rel="stylesheet" href="Styles/General.css">
+	<link rel="stylesheet" href="Styles/Loader.css">
+	<link rel="stylesheet" href="Styles/Light.css">
+	<link rel="stylesheet" href="Styles/Dark.css">
+	<link rel="preconnect" href="https://fonts.gstatic.com">
+	<link rel="shortcut icon" href="#">
+	<link rel='stylesheet' type='text/css' href='https://fonts.googleapis.com/css?family=Lato:400,700'>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/core-js/2.6.10/shim.js"></script>
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js"></script>
+	<script type="module" src="Misc/Extensions.js"></script>
+	<script type="module" src="Visual/Theme.js" defer></script>
+	<script type="module" src=App.js defer></script>
+	<script src='//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
+	<script src='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js'></script>
+</head>
 
-        if (currentIndex > message.length) {
-            clearInterval(typingInterval);
-            container.scrollTop = container.scrollHeight;
-        }
-    }, timeout);
-}
+<body>
+	<div class="avatar unselectable">
+		<div class="video-container">
+			<!-- autoplay loop muted -->
+			<video autoplay muted id="video-player" preload="auto" poster="Videos/Poster.png">
+				<source src="Videos/Intro.mp4" type="video/mp4">
+				Your browser does not support the video tag.
+			</video>
+		</div>
+		<div class="settings-wrapper">
+			<div class="dropdown popout-left pull-right">
+				<button class="window-button" type="button" role="button" id="nav-menu" data-toggle="dropdown"
+					aria-expanded="true">
+					<span class="icon-popout-left glyphicon glyphicon-cog"></span>
+				</button>
+				<ul class="dropdown-menu popout-menu-left" role="menu" aria-labelledby="nav-menu">
+					<li role="presentation" class="dropdown-link-left arrow-box-right">
+						<a role="menuitem" id="dark" class="dark toggle d-none settings">
+							<i class="fas fa-moon" title="Dark Mode"></i>
+						</a>
+						<a role="menuitem" id="light" class="light toggle settings">
+							<i class="fas fa-sun" title="Light Mode"></i>
+						</a>
+					</li>
+					<li role="presentation" class="dropdown-link-left arrow-box-right">
+						<a role="menuitem" class="glyphicon glyphicon-globe settings toggle">
+						</a>
+					</li>
+				</ul>
+			</div>
+		</div>
+	</div>
+	<div class="chat unselectable">
+		<div id="chat" class="out-container">
+			<div class="out">Ciao, sono Federico II e ti aiuterò in questo percorso</div>
+		</div>
+		<div class="text">
+			<div id="mic-wrapper" class="mic-wrapper">
+				<div class="mic">
+					<span id="mic" class="material-symbols-outlined mic">
+						mic
+					</span>
+				</div>
+			</div>
+			<div class="text-input-wrapper">
+				<input id="text-input" class="text-input" placeholder="Type here...">
+			</div>
+			<div class="send-button-wrapper">
+				<span id="send-button" class="material-symbols-outlined send">
+					send
+				</span>
+			</div>
+		</div>
+	</div>
 
+	<div class="toolkit unselectable">
+		<span id="clear" class="glyphicon glyphicon-remove-circle tools">
+		</span>
+		<span class="glyphicon glyphicon-question-sign tools">
+		</span>
+	</div>
+	<div class="loader-wrapper">
 
-// Submit input
-function submitInput() {
-    const messageContent = inputField.value.trim();
-    let answerProvided = false;
-    if (messageContent !== "") {
-        inputField.value = "";
-        typingMessage(chatContainer, messageContent, 20);
-        messageContent.split(' ').forEach(word => {
-            word = word.match(lettersRegex)?.[0]?.toLowerCase();
-            if (word) {
-                if (repeatKeywords.includes(word)) {
-                    videoPlayer.currentTime = 0;
-                    videoPlayer.play();
-                    answerProvided = true;
-                    typingMessage(chatContainer, currentTopic, 50);
-                    return;
-                }
-                for (let index = 0; index < italianKeywords.length; index++) {
-                    if (italianKeywords[index].includes(word)) {
-                        videoPlayer.src = italianVideos[index];
-                        fetch(italianContents[index])
-                            .then((res) => res.text())
-                            .then((text) => {
-                                currentTopic = text;
-                                typingMessage(chatContainer, currentTopic, 50);
-                            })
-                            .catch((e) => console.error(e));
-                        answerProvided = true;
-                        return;
-                    }
-                }
-            }
-        });
-        if (!answerProvided) {
-            typingMessage(chatContainer, noAnswer, 30);
-        }
-    }
-}
+		<div class="socket">
+			<div class="gel c1 r1">
+				<div class="hex-brick h1"></div>
+				<div class="hex-brick h2"></div>
+				<div class="hex-brick h3"></div>
+			</div>
+			<div class="gel c2 r1">
+				<div class="hex-brick h1"></div>
+				<div class="hex-brick h2"></div>
+				<div class="hex-brick h3"></div>
+			</div>
+			<div class="gel c3 r1">
+				<div class="hex-brick h1"></div>
+				<div class="hex-brick h2"></div>
+				<div class="hex-brick h3"></div>
+			</div>
+			<div class="gel c4 r1">
+				<div class="hex-brick h1"></div>
+				<div class="hex-brick h2"></div>
+				<div class="hex-brick h3"></div>
+			</div>
+			<div class="gel c5 r1">
+				<div class="hex-brick h1"></div>
+				<div class="hex-brick h2"></div>
+				<div class="hex-brick h3"></div>
+			</div>
+			<div class="gel c6 r1">
+				<div class="hex-brick h1"></div>
+				<div class="hex-brick h2"></div>
+				<div class="hex-brick h3"></div>
+			</div>
 
-// Chat
-document.addEventListener("DOMContentLoaded", () => {
-    inputField.addEventListener("keyup", (event) => {
-        if (event.key === "Enter") {
-            submitInput();
-        }
-    });
-})
+			<div class="gel c7 r1">
+				<div class="hex-brick h1"></div>
+				<div class="hex-brick h2"></div>
+				<div class="hex-brick h3"></div>
+			</div>
 
-document.addEventListener("DOMContentLoaded", () => {
-    sendButton.addEventListener("click", () => {
-        submitInput();
-    });
-})
+			<div class="gel c8 r1">
+				<div class="hex-brick h1"></div>
+				<div class="hex-brick h2"></div>
+				<div class="hex-brick h3"></div>
+			</div>
+			<div class="gel c9 r2">
+				<div class="hex-brick h1"></div>
+				<div class="hex-brick h2"></div>
+				<div class="hex-brick h3"></div>
+			</div>
+			<div class="gel c10 r2">
+				<div class="hex-brick h1"></div>
+				<div class="hex-brick h2"></div>
+				<div class="hex-brick h3"></div>
+			</div>
+			<div class="gel c11 r2">
+				<div class="hex-brick h1"></div>
+				<div class="hex-brick h2"></div>
+				<div class="hex-brick h3"></div>
+			</div>
+			<div class="gel c12 r2">
+				<div class="hex-brick h1"></div>
+				<div class="hex-brick h2"></div>
+				<div class="hex-brick h3"></div>
+			</div>
+			<div class="gel c13 r2">
+				<div class="hex-brick h1"></div>
+				<div class="hex-brick h2"></div>
+				<div class="hex-brick h3"></div>
+			</div>
+			<div class="gel c14 r2">
+				<div class="hex-brick h1"></div>
+				<div class="hex-brick h2"></div>
+				<div class="hex-brick h3"></div>
+			</div>
+			<div class="gel c15 r2">
+				<div class="hex-brick h1"></div>
+				<div class="hex-brick h2"></div>
+				<div class="hex-brick h3"></div>
+			</div>
+			<div class="gel c16 r2">
+				<div class="hex-brick h1"></div>
+				<div class="hex-brick h2"></div>
+				<div class="hex-brick h3"></div>
+			</div>
+			<div class="gel c17 r2">
+				<div class="hex-brick h1"></div>
+				<div class="hex-brick h2"></div>
+				<div class="hex-brick h3"></div>
+			</div>
+			<div class="gel c18 r3">
+				<div class="hex-brick h1"></div>
+				<div class="hex-brick h2"></div>
+				<div class="hex-brick h3"></div>
+			</div>
+			<div class="gel c19 r3">
+				<div class="hex-brick h1"></div>
+				<div class="hex-brick h2"></div>
+				<div class="hex-brick h3"></div>
+			</div>
+			<div class="gel c20 r3">
+				<div class="hex-brick h1"></div>
+				<div class="hex-brick h2"></div>
+				<div class="hex-brick h3"></div>
+			</div>
+			<div class="gel c21 r3">
+				<div class="hex-brick h1"></div>
+				<div class="hex-brick h2"></div>
+				<div class="hex-brick h3"></div>
+			</div>
+			<div class="gel c22 r3">
+				<div class="hex-brick h1"></div>
+				<div class="hex-brick h2"></div>
+				<div class="hex-brick h3"></div>
+			</div>
+			<div class="gel c23 r3">
+				<div class="hex-brick h1"></div>
+				<div class="hex-brick h2"></div>
+				<div class="hex-brick h3"></div>
+			</div>
+			<div class="gel c24 r3">
+				<div class="hex-brick h1"></div>
+				<div class="hex-brick h2"></div>
+				<div class="hex-brick h3"></div>
+			</div>
+			<div class="gel c25 r3">
+				<div class="hex-brick h1"></div>
+				<div class="hex-brick h2"></div>
+				<div class="hex-brick h3"></div>
+			</div>
+			<div class="gel c26 r3">
+				<div class="hex-brick h1"></div>
+				<div class="hex-brick h2"></div>
+				<div class="hex-brick h3"></div>
+			</div>
+			<div class="gel c27 r3">
+				<div class="hex-brick h1"></div>
+				<div class="hex-brick h2"></div>
+				<div class="hex-brick h3"></div>
+			</div>
+			<div class="gel c28 r4">
+				<div class="hex-brick h1"></div>
+				<div class="hex-brick h2"></div>
+				<div class="hex-brick h3"></div>
+			</div>
+			<div class="gel c29 r4">
+				<div class="hex-brick h1"></div>
+				<div class="hex-brick h2"></div>
+				<div class="hex-brick h3"></div>
+			</div>
+			<div class="gel c30 r4">
+				<div class="hex-brick h1"></div>
+				<div class="hex-brick h2"></div>
+				<div class="hex-brick h3"></div>
+			</div>
+			<div class="gel c31 r4">
+				<div class="hex-brick h1"></div>
+				<div class="hex-brick h2"></div>
+				<div class="hex-brick h3"></div>
+			</div>
+			<div class="gel c32 r4">
+				<div class="hex-brick h1"></div>
+				<div class="hex-brick h2"></div>
+				<div class="hex-brick h3"></div>
+			</div>
+			<div class="gel c33 r4">
+				<div class="hex-brick h1"></div>
+				<div class="hex-brick h2"></div>
+				<div class="hex-brick h3"></div>
+			</div>
+			<div class="gel c34 r4">
+				<div class="hex-brick h1"></div>
+				<div class="hex-brick h2"></div>
+				<div class="hex-brick h3"></div>
+			</div>
+			<div class="gel c35 r4">
+				<div class="hex-brick h1"></div>
+				<div class="hex-brick h2"></div>
+				<div class="hex-brick h3"></div>
+			</div>
+			<div class="gel c36 r4">
+				<div class="hex-brick h1"></div>
+				<div class="hex-brick h2"></div>
+				<div class="hex-brick h3"></div>
+			</div>
+			<div class="gel c37 r4">
+				<div class="hex-brick h1"></div>
+				<div class="hex-brick h2"></div>
+				<div class="hex-brick h3"></div>
+			</div>
 
-// Clear content
-document.addEventListener("DOMContentLoaded", () => {
-    clearButton.addEventListener("click", () => {
-        chatContainer.innerHTML = "";
-        videoPlayer.pause();
-        videoPlayer.currentTime = 0;
-        typingMessage(chatContainer, greeting, 10);
-    });
-});
+			<div class="gel c38 r4">
+				<div class="hex-brick h1"></div>
+				<div class="hex-brick h2"></div>
+				<div class="hex-brick h3"></div>
+			</div>
+			<div class="gel c39 r4">
+				<div class="hex-brick h1"></div>
+				<div class="hex-brick h2"></div>
+				<div class="hex-brick h3"></div>
+			</div>
+			<div class="gel c40 r4">
+				<div class="hex-brick h1"></div>
+				<div class="hex-brick h2"></div>
+				<div class="hex-brick h3"></div>
+			</div>
+			<div class="gel c41 r4">
+				<div class="hex-brick h1"></div>
+				<div class="hex-brick h2"></div>
+				<div class="hex-brick h3"></div>
+			</div>
+			<div class="gel c42 r4">
+				<div class="hex-brick h1"></div>
+				<div class="hex-brick h2"></div>
+				<div class="hex-brick h3"></div>
+			</div>
+		</div>
+	</div>
 
+</body>
 
-// Loading animation
-$(window).on("load", function () { $(".loader-wrapper").fadeOut("slow"); });
-
-
-// Drop down pop up
-$('.popout-left').on('show.bs.dropdown', function () {
-    $('.window-button>.icon-popout-left').removeClass('glyphicon-cog');
-    $('.window-button>.icon-popout-left').addClass('animated rotateIn glyphicon-remove');
-    $('.dropdown-link-left').addClass('animated bounceIn');
-})
-$('.popout-left').on('hide.bs.dropdown', function () {
-    $('.window-button>.icon-popout-left').removeClass('animated rotateIn glyphicon-remove');
-    $('.dropdown-link-left').removeClass('animated bounceIn');
-    $('.window-button>.icon-popout-left').addClass('animated bounceIn glyphicon-cog');
-})
+</html>
